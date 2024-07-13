6d7c390830b806a989d6bc9ac6313597c92e1286
1zumisawashun <shun.1zumisawa@gmail.com>
2024-07-13 18:20:34 +0900

    storybook-addon-pseudo-statesの導入

--- app/components/Button/index.stories.tsx
+++ app/components/Button/index.stories.tsx
@@ -1,31 +1,43 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import { fn } from "@storybook/test";
 import { Button } from "./index";
+import { ComponentProps } from "react";
+import { HStack } from "@chakra-ui/react";
 
-// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
   title: "Button",
   component: Button,
-  parameters: {
-    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
-    layout: "centered",
-  },
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
-  tags: ["autodocs"],
-  // More on argTypes: https://storybook.js.org/docs/api/argtypes
-  argTypes: {
-    backgroundColor: { control: "color" },
-  },
-  // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
-  args: { onClick: fn() },
 } satisfies Meta<typeof Button>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+const Render = (props: ComponentProps<typeof Button>) => {
+  return (
+    <HStack>
+      <Button {...props}>default</Button>
+      <Button id="hover" {...props}>
+        hover
+      </Button>
+      <Button id="active" {...props}>
+        active
+      </Button>
+      <Button id="focus-visible" {...props}>
+        focus-visible
+      </Button>
+    </HStack>
+  );
+};
 export const Default: Story = {
   args: {
     children: "Button",
+    colorScheme: "blue",
   },
+  parameters: {
+    pseudo: {
+      hover: "#hover",
+      active: "#active",
+      focusVisible: "#focus-visible",
+    },
+  },
+  render: Render,
 };
