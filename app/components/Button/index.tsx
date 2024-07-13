b87f49424e406eec642029196929fad6123a9eb6
1zumisawashun <shun.1zumisawa@gmail.com>
2024-07-13 10:12:19 +0900

    chore : chakra-uiの導入

---
+++ app/components/Button/index.tsx
@@ -0,0 +1,11 @@
+// app/page.tsx
+'use client'
+import { Button as RowButton } from '@chakra-ui/react'
+
+export const Button:React.FC=()=> {
+  return (
+    <RowButton>
+      About
+    </RowButton>
+  )
+}
---
+++ app/components/Link/index.tsx
@@ -0,0 +1,11 @@
+// app/page.tsx
+'use client'
+import { Link as RowLink } from '@chakra-ui/next-js'
+
+export const Link:React.FC=()=> {
+  return (
+    <RowLink href='/about' color='blue.400' _hover={{ color: 'blue.500' }}>
+      About
+    </RowLink>
+  )
+}
